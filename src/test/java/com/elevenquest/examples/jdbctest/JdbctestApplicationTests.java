2bc815c820758c8a35f2a79d1fe2d5b9c3159f7e
toheavener01@gmail.com <toheavener01@gmail.com>
2020-08-05 16:39:43 +0900

    initial commit

---
+++ src/test/java/com/elevenquest/examples/jdbctest/JdbctestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elevenquest.examples.jdbctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbctestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
